0e2fe20bfd8655de75fbaaef561354fa58922129
Aleksandar Marojevic <acomarojevic5@gmail.com>
2023-09-17 23:21:53 +0200

    sredjivanje koda i komentara

--- server/express.js
+++ server/express.js
@@ -27,25 +27,24 @@ import devBundle from './devBundle'
 const CURRENT_WORKING_DIR = process.cwd()
 const app = express()
 
-//comment out before building for production
+
 devBundle.compile(app)
 
-// parse body params and attache them to req.body
+
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cookieParser())
 app.use(compress())
-// secure apps by setting various HTTP headers
+
 app.use(helmet())
-// enable CORS - Cross Origin Resource Sharing
+
 app.use(cors())
 
 app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR, 'dist')))
 
-// mount routes
-//LOGIKA
+
+
 app.use('/', userRoutes)
-//autentifikacija iliti login
 app.use('/', authRoutes)
 app.use('/', courseRoutes)
 app.use('/', enrollmentRoutes)
@@ -72,7 +71,7 @@ app.get('*', (req, res) => {
     }))
 })
 
-// Catch unauthorised errors
+
 app.use((err, req, res, next) => {
   if (err.name === 'UnauthorizedError') {
     res.status(401).json({"error" : err.name + ": " + err.message})
--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ import config from './../config/config'
 import app from './express'
 import mongoose from 'mongoose'
 
-// Connection URL
+
 mongoose.Promise = global.Promise
 mongoose.connect("mongodb+srv://novi-korisnik-1:novi-korisnik-1@cluster0.jlb4z0a.mongodb.net/?retryWrites=true&w=majority", { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
 mongoose.connection.on('error', () => {
